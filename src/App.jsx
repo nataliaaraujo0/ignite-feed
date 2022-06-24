f9497677213c257e7f103d71e1c59a9c7383739e
natalia.araujo <natalia.araujo@yazo.com.br>
2022-06-24 15:17:48 -0300

    chore: finish sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
 import { Header } from "./components/Header";
 import "../styles/global.scss";
+import styles from "./app.module.scss";
+import { Post } from "./components/Post";
+import { Sidebar } from "./components/Sidebar";
 function App() {
   return (
     <>
       <Header />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Naruto"
+            content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi dolorum nesciunt dolore repellendus, modi, voluptatum qui officia dignissimos nulla aliquam molestias ipsa eveniet id esse tenetur ad nostrum libero consequatur!"
+          />
+        </main>
+      </div>
     </>
   );
 }
