ea3aae054dfe2258a7aa28ed9836211043a2d349
natalia.araujo <natalia.araujo@yazo.com.br>
2022-06-26 16:38:17 -0300

    chore: finish comment  to post

--- src/components/Post/index.jsx
+++ src/components/Post/index.jsx
@@ -30,6 +30,14 @@ export function Post() {
           <a href="">#novoprojeto #nlw #rocketseat</a>
         </p>
       </div>
+
+      <form className={styles.comentForm}>
+        <strong>Deixe seu feedback</strong>
+        <textarea placeholder="Deixe um comentário" />
+        <footer>
+          <button type="submit">Publicar</button>
+        </footer>
+      </form>
     </article>
   );
 }
